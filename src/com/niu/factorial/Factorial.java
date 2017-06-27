16601be1932da779f9fe75c8cb76ca703a3c95c1
牛浩轩 <niuhaoxuan@bonc.com.cn>
2017-06-27 15:00:07 +0800

    阶乘递归计算

---
+++ src/com/niu/factorial/Factorial.java
@@ -0,0 +1,29 @@
+package com.niu.factorial;
+
+import java.util.Scanner;
+
+/**
+ * 
+ * 提示用户输入一个非负整数，然后显示这个非负整数的阶乘
+ *
+ * @author 牛浩轩
+ * @version 2017年6月27日
+ * @see Factorial
+ * @since
+ */
+public class Factorial {
+    public void result(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入一个非负整数");
+        int num = input.nextInt();
+        System.out.println(num + "的阶乘是：" + multiplication(num));
+    }
+    
+    public long multiplication(int n){
+        if(n == 0){
+            return 1;
+        }else{
+            return n * multiplication(n - 1);
+        }
+    }
+}
--- src/com/niu/fream/FreamApplication.java
+++ src/com/niu/fream/FreamApplication.java
@@ -1,17 +1,32 @@
 package com.niu.fream;
 
+import com.niu.factorial.Factorial;
 import com.niu.iteration.Iteration;
 
+/**
+ * 
+ * 主函数，各种功能的启动类
+ *
+ * @author 牛浩轩
+ * @version 2017年6月27日
+ * @see FreamApplication
+ * @since
+ */
 public class FreamApplication {
     public static void main(String[] args) {
         /**
          * 迭代计算文本中的“A”
          */
-        System.out.println(new Iteration().countA("AoefkopsemfIJIOAAA")); 
+        //System.out.println(new Iteration().countA("AoefkopsemfIJIOAAA")); 
         
         /**
          * 递归计算文本中的“A”
          */
-        System.out.println(new Iteration().countB("AAA rating"));
+        //System.out.println(new Iteration().countB("AAA rating"));
+        
+        /**
+         * 提示用户输入一个非负整数，然后显示这个非负整数的阶乘
+         */
+        new Factorial().result();
     }
 }
