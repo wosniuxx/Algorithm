60d46183b48551c1f7a11cedddb8ffda7460ebd0
wosniuxx <wosniuxx@163.com>
2018-06-15 11:27:35 +0800

    提交1

--- src/com/niu/jdk8/test.java
+++ src/com/niu/jdk8/test.java
@@ -8,7 +8,7 @@ package com.niu.jdk8;
 public class test {
 
     public static void main(String[] args) {
-        System.out.println("1");
+        System.out.println("test");
         System.out.println("1");
     }
 
