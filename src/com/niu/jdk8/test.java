77a9c4bc1c85b7c57f7196d1d054ae8cf36265b5
wosniuxx <wosniuxx@163.com>
2018-06-15 11:14:58 +0800

    testsout1

---
+++ src/com/niu/jdk8/test.java
@@ -0,0 +1,14 @@
+package com.niu.jdk8;
+
+/**
+ * @description: ${description}
+ * @author: Niu Haoxuan
+ * @create: 2018-06-15 11:14
+ **/
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+
+}
