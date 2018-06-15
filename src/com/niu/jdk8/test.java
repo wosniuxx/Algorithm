627dc8301e09e52b89bd516b8b8ddd87e8b82391
wosniuxx <wosniuxx@163.com>
2018-06-15 11:27:53 +0800

    提交3

--- src/com/niu/jdk8/test.java
+++ src/com/niu/jdk8/test.java
@@ -10,6 +10,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("test");
         System.out.println("test");
+        System.out.println("test");
     }
 
 }
